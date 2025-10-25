74d7dc78163db83d89d02bb72f625900f5697da7
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-10-25 23:33:53 +0900

    feat: implement basic dashboard layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Dashboard.scss";
+import ChartCard from "@/components/Card/ChartCard";
 
 const Dashboard: React.FC = () => {
-  return <div> 대시보드 화면입니다.</div>;
+  return (
+    <div className="dashboard">
+      <div className="dashboard__grid">
+        <ChartCard title="PGA / SGA 압박률" />
+        <ChartCard title="Wait Class 분포" />
+        <ChartCard title="세션 한도 상태" />
+        <ChartCard title="핵심 테이블스페이스 여유율" />
+        <ChartCard title="백그라운드 프로세스 상태" />
+        <ChartCard title="제한 근접 파라미터 상태" />
+        <ChartCard title="CPU 사용" />
+        <ChartCard title="I/O 지연량" status="warning" />
+        <ChartCard title="I/O 처리량" />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
