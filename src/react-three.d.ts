3cd315a189a0c898a005a12a46f8f467d1d848d9
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-11-17 03:02:21 +0900

    fix: resolve TypeScript project reference errors by enabling composite builds

--- src/react-three.d.ts
+++ src/react-three.d.ts
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "@react-three/fiber";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as THREE from "three";
 
 declare global {
   namespace JSX {
