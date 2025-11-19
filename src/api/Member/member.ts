dbc63274a9d7c30f8a30432c6306fddf88595f50
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-11-19 09:47:39 +0900

    feat: integrate member update API to setting page

--- src/api/Member/member.ts
+++ src/api/Member/member.ts
@@ -13,8 +13,20 @@ export interface Member {
   updatedAt: string;
 }
 
+export interface UpdateMemberRequest {
+  username: string;
+  email: string;
+  company: string;
+}
+
 // 전체 회원 조회 API
 export const fetchMembers = async (): Promise<Member[]> => {
-  const res = await axios.get("http://localhost:8080/api/members");
+  const res = await axios.get("/api/members");
   return res.data.data;
+};
+
+// 회원 수정 API
+export const updateMember = async (id: number, body: UpdateMemberRequest) => {
+  const res = await axios.put(`/api/members/${id}`, body);
+  return res.data;
 };
